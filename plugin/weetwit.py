7001f7cf7c4ce028399b3aa3d59cee94bd1b6927
Daniël Franke <daniel.franke@booking.com>
2014-03-04 10:11:30 +0100

    s/$/%/

--- plugin/weetwit.py
+++ plugin/weetwit.py
@@ -204,7 +204,7 @@ def display_cb(data, remaining_calls):
                 if tweet.is_retweet:
                     retweeter = "%s@%s" % (mention_color, tweet.rtscreen_name)
                     if retweet_style == 'postfix':
-                        output = '%s\t%s%s (RT by %s$s)' % (screen_name,
+                        output = '%s\t%s%s (RT by %s%s)' % (screen_name,
                                                             text_color,
                                                             text,
                                                             retweeter,
